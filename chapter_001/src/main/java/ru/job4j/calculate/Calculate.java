32bddb081c7b05a6d2e630f577bd1b155aaaa9b0
Antierror <plan.05.87@gmail.com>
2018-10-23 00:35:46 +0300

    1.3. JavaDoc. Code convention [#77365]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ *
+ * @author Konstantin Kazakov (plan.05.87@gmail.com).
+ */
 public class Calculate {
-	
+	/**
+	 * Main.
+	 * @param args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
