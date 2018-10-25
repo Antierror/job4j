bb5383b8869a8230323ce9f3bb736705754c25be
Antierror <plan.05.87@gmail.com>
2018-10-25 23:16:16 +0300

    2.2. Импорт Maven проекта.[#77320]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,9 +8,9 @@ package ru.job4j.calculate;
 public class Calculate {
 	/**
 	 * Main.
-	 * @param args.
+	 * @param args - args.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) {   
 		System.out.println("Hello World");
 	}
 	
