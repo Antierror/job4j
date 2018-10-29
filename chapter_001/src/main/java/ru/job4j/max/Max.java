ff3149bacc93a90e408603ba59d690e2790643a9
Antierror <plan.05.87@gmail.com>
2018-10-29 21:12:37 +0300

    4.2. Максимум из двух чисел.

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.max;
+
+/**
+ * @author Konstantin Kazakov (plan.05.87@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Программа определющий максимус из дыух яичел.
+     * @param first Первое число.
+     * @param second Второе число.
+     * @return Максимальное число.
+     */
+    public int max(int first, int second) {
+    return first > second ? first : second;
+    }
+}
