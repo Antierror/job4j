92d5ac1b835f66b9b444380cc955b5332d3abe9e
Antierror <plan.05.87@gmail.com>
2018-11-05 21:31:15 +0300

    5.5. Рефакторинг кода.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,11 +1,35 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * @author Konstantin Kazakov(email: plan.05.87@gmail.com)
  * @version $Id$
  * @since 0.1
  */
 public class Paint {
+
+    /**
+     * Метод для .
+     * @param height Высота пирамиды
+     * @param weight Ширина пирамиды
+     * @return Рисунок пирамиды.
+     */
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
+        StringBuilder screen = new StringBuilder();
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (predict.test(row, column)) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
     /**
      * Метод для построения пирамиды в псевдографике.
      *
@@ -18,20 +42,11 @@ public class Paint {
      * @return Рисунок пирамиды.
      */
     public String piramid(int h) {
-        StringBuilder screen = new StringBuilder();
-        //Размер weight равен двум высотам, чтоб центр был одним то нужно -1.
-        int weight = 2 * h - 1;
-        for (int row = 0; row != h; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= h - column - 1 && row + h - 1 >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                h,
+                h * 2 - 1,
+                (row, column) -> row >= h - column - 1 && row + h - 1 >= column
+        );
     }
     /**
      * Метод для построения правого треугольника в псевдографике.
@@ -45,20 +60,11 @@ public class Paint {
      * @return Рисунок правого треугольника.
      */
     public String rightTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        //weight - ширина равно высоте.
-        int weight = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
     /**
      * Метод для построения левого треугольника в псевдографике.
@@ -72,19 +78,10 @@ public class Paint {
      * @return Рисунок левого трегольника.
      */
     public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int weight = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= weight - column - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column -1
+        );
     }
-
 }
