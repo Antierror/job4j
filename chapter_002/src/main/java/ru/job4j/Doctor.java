33c711771ae8db32336eecff3e53951dc74c6b62
Antierror <plan.05.87@gmail.com>
2018-11-21 21:28:57 +0300

    1. Создать классы - профессий

---
+++ chapter_002/src/main/java/ru/job4j/Doctor.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Doctor extends Profession {
+    public void treatsPatient() {
+        Patient patient = new Patient();
+        patient.name = "Vova";
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/Profession.java
@@ -0,0 +1,6 @@
+package ru.job4j;
+
+public class Profession {
+    public String name;
+    public String profession;
+}
