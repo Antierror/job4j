77309e4606efc834f1e7b69e44086d587afefed9
Antierror <plan.05.87@gmail.com>
2018-11-21 22:05:12 +0300

    Добавление JavaDoc

--- chapter_002/src/main/java/ru/job4j/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/Doctor.java
@@ -1,5 +1,10 @@
 package ru.job4j;
 
+/**
+ * @author Konstantin Kazakov (plan.05.87@gmail.com).
+ * @version $Id$
+ * @since 0.1
+ */
 public class Doctor extends Profession {
     public void treatsPatient() {
         Patient patient = new Patient();
--- chapter_002/src/main/java/ru/job4j/House.java
+++ chapter_002/src/main/java/ru/job4j/House.java
@@ -1,4 +1,9 @@
 package ru.job4j;
 
+/**
+ * @author Konstantin Kazakov (plan.05.87@gmail.com).
+ * @version $Id$
+ * @since 0.1
+ */
 public class House {
 }
