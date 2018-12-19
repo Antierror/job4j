cd43793cbfa67b5b7a94c2f3f8202e055ea0db47
Antierror <plan.05.87@gmail.com>
2018-12-19 07:53:18 +0300

    Доп.задание на вопросы базовый синтаксис.

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
 
+import static java.lang.System.arraycopy;
+
 /**
  * @author Konstantin Kazakov (Email: plan.05.87@gmail.com)
  * @version $Id$
@@ -8,6 +10,7 @@ package ru.job4j.array;
 public class BubbleSort {
     /**
      * Пузырьковый метод по сортировке массива.
+     *
      * @param array Входящий массив
      * @return Возвращение отсортированный массив.
      */
@@ -23,4 +26,42 @@ public class BubbleSort {
         }
         return array;
     }
+    /**
+     *  Проверка отсортированного массива.
+     * @param array Входящий массив
+     * @return Возврат проверки отсортированного массива.
+     */
+    public boolean checkSort (int[] array) {
+        boolean result = true;
+        for (int i = 1; i < array.length - 1; i++)
+            /*
+            Если первый элемент больше следушего, то результат будет отрицательный.
+             */
+            if (array[i] > array[i + 1]) {
+                result = false;
+                break;
+            }
+        return result;
+    }
+    /**
+     * Данны два массива отсортированных по возрастанию.
+     * Необходимо получить третий массив и отсортировать его по возрастанию.
+     * @param array1 Первый входящий массив
+     * @param array2 Второй входящий массив
+     * @return array3 Возврат третьего отсортированного массива из слитых двух.
+     */
+    public int[] sortMerge(int[] array1, int[] array2) {
+        int[] array3 = new int[array1.length + array2.length];
+        for (int i = 0, j = 0, k = 0; k < array3.length;) {
+            while (i < array1.length && j < array2.length) {
+                array3[k++] = array1[i] <= array2[j] ? array1[i++] : array2[j++];
+            }
+            while (i < array1.length)
+                array3[k++] = array1[i++];
+            while (j < array2.length)
+                array3[k++] = array2[j++];
+        }
+        return array3;
+    }
 }
+
