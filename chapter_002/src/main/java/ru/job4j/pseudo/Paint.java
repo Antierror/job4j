6b78d8309acfcd930a4f7666a5983325ae909a44
Antierror <plan.05.87@gmail.com>
2019-01-21 16:13:12 +0300

    5. Рефакторинг теста @Before @After

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -0,0 +1,12 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Konstantin Kazakov (plan.05.87@gmail.com).
+ * @version $Id$
+ * @since 0.1
+ */
+public class Paint {
+    public void draw(Shape shape) {
+        System.out.println(shape.draw());
+    }
+}
